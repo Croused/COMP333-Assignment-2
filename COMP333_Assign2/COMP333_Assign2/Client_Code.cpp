05ee99111a20400c6e7cc2161213ee1b47bb2e5d
Darrell Crouse <crouse.darrell@gmail.com>
2019-11-08 15:39:02 -0500

    Coded polymorphic funcion showInfo() as requested. Added Donna's Client code I had to update Donna's code to call the correct method.  (getQOH to getQtyOnHand)

--- COMP333_Assign2/COMP333_Assign2/Client_Code.cpp
+++ COMP333_Assign2/COMP333_Assign2/Client_Code.cpp
@@ -3,33 +3,109 @@
 
 
 #include <iostream>
-#include "VendingMachine.h";
+#include <iomanip>
+#include <cassert>
+#include "VendingMachine.h"
 #include "SodaPop.h"
-#include "CandyMachine.h";
+#include "CandyMachine.h"
 
 using namespace std;
 
-int main() {
-
-	VendingMachine obj1("prod", 1);
-	SodaPop obj2(1, "pop", 2.0);
-	CandyMachine obj3(1, "Candy", 3.2);
-
-	obj1.show(cout);
-	obj2.show(cout);
-	obj2.buyPop();
-	obj2.show(cout);
-	obj2.buyPop();
-	obj2.show(cout);
-
-	obj3.show(cout);
-	obj3.buyPop();
-	obj3.show(cout);
-	obj3.buyPop();
-	obj3.show(cout);
+void showInfo(const VendingMachine* /*Vending Object*/);
 
 
 
-	cout << "\n\nEnd Of Program\n";
+int main()
+{
+	cout << fixed << setprecision(2);
+
+	cout << "1. Demonstrating creating a SodaPop on the stack, calling upon the non default constructor\n";
+	SodaPop drinkMachine1(1, "Diet Coke", 2.49);
+	cout << "Drink machine#1 is selling: "
+		<< drinkMachine1.getProductName();
+	cout << "\n selling price is $: "
+		<< drinkMachine1.getPrice();
+	cout << "\n quantity on hand = " << drinkMachine1.getQtyOnHand() << "\n\n";
+
+	cout << "2. Demonstrating creating a SodaPop object on the stack, calling non default constructor with no product name \n";
+	SodaPop drinkMachine2(100, "", 1.00);
+	cout << "No product name was assigned for Drink Machine#2, so it is selling: "
+		<< drinkMachine2.getProductName();
+	cout << "\n selling price is $: "
+		<< drinkMachine2.getPrice();
+	cout << "\n quantity on hand = " << drinkMachine2.getQtyOnHand() << "\n\n";
+
+
+	cout << "3. Demonstrating creating a SodaPop object on the heap, calling non default with no price\n";
+	SodaPop * drinkMachine3 = new SodaPop(53, "Ginger Ale", 0.0);
+	assert(drinkMachine3);
+	cout << "Drink machine#3 is selling: "
+		<< drinkMachine3->getProductName();
+	cout << "\n since no price was assigned, it’s selling price is $: "
+		<< drinkMachine3->getPrice();
+	cout << "\n quantity on hand = " << drinkMachine3->getQtyOnHand() << "\n\n";
+
+
+	cout << "4. Demonstrating creating a SodaPop object on the heap, calling default constructor\n";
+	SodaPop *drinkMachine4 = new SodaPop;
+	assert(drinkMachine4);
+	cout << "Drink machine#4 is selling: "
+		<< drinkMachine4->getProductName();
+	cout << "\n selling price is $: "
+		<< drinkMachine4->getPrice();
+	cout << "\n quantity on hand = " << drinkMachine4->getQtyOnHand() << "\n\n";
+
+	cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n";
+
+	cout << "5. Demonstrating creating a CandyMachine object on the stack, calling non default constructor\n";
+	CandyMachine candyDispenser1(1.79, "Almond Joy", 40);
+	cout << "Candy machine#1 is selling: "
+		<< candyDispenser1.getProductName();
+	cout << "\n selling price is $: "
+		<< candyDispenser1.getPrice();
+	cout << "\n quantity on hand = " << candyDispenser1.getQtyOnHand() << "\n\n";
+
+	cout << "6. Demonstrating creating a CandyMachine object on the heap, calling non default constructor\n";
+	CandyMachine * candyDispenser2 = new CandyMachine(.75, "Hersheys", 30);
+	assert(candyDispenser2);
+	cout << "Candy machine#2 is selling: "
+		<< candyDispenser2->getProductName();
+	cout << "\n selling price is $: "
+		<< candyDispenser2->getPrice();
+	cout << "\n quantity on hand = " << candyDispenser2->getQtyOnHand() << "\n\n";
+
+	cout << "7. Demonstrating creating a CandyMachine object on the heap, calling default constructor\n";
+	CandyMachine * candyDispenser3 = new CandyMachine;
+	assert(candyDispenser3);
+	cout << "Candy machine#2 is selling: "
+		<< candyDispenser3->getProductName();
+	cout << "\n selling price is $: "
+		<< candyDispenser3->getPrice();
+	cout << "\n quantity on hand = " << candyDispenser3->getQtyOnHand() << "\n\n";
+
+	cout << endl << endl;
+
+	//	call polymorphic function showing costs of candyDispenser and drinkMachine objects
+	cout << "9. Demonstrating calling polymorphic function\n";
+	showInfo(&drinkMachine1);
+	showInfo(&drinkMachine2);
+	showInfo(drinkMachine3);
+	showInfo(drinkMachine4);
+	showInfo(&candyDispenser1);
+	showInfo(candyDispenser2);
+	showInfo(candyDispenser3);
+
+
+
 	system("pause");
+}
+
+
+
+//
+
+void showInfo(const VendingMachine *VendObj) {
+	VendObj->show(cout);
+	cout << '\n';
+
 }
--- COMP333_Assign2/COMP333_Assign2/VendingMachine.h
+++ COMP333_Assign2/COMP333_Assign2/VendingMachine.h
@@ -18,7 +18,7 @@ class VendingMachine {
 		VendingMachine(string, /*Initial Product Name*/
 					   int /*Inital Quantity On Hand*/);
 		~VendingMachine();
-		void show(ostream & /*Output Source*/) const;
+		virtual void show(ostream & /*Output Source*/) const;
 		int getQtyOnHand() const { return qtyOnHand; }
 		string getProductName() const { return *productName; };
 		
